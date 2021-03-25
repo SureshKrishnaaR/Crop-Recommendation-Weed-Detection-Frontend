53f3930ced821d865eba3beaaac04559cdd57722
Suresh Krishnaa R <progrsk24@gmail.com>
2021-03-25 23:51:05 +0530

    add homepage to the application

---
+++ src/Pages/HomePage/homepage.container.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import HomePageView from "./homepage.view.jsx";
+
+const HomePage = () => {
+  return (
+    <>
+      <HomePageView />
+    </>
+  );
+};
+
+export default HomePage;
---
+++ src/Pages/HomePage/index.js
@@ -0,0 +1,3 @@
+import HomePage from "./homepage.container.jsx";
+
+export default HomePage;
