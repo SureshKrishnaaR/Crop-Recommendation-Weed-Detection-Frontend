569307f0f1912963982722442fa1fb1d95f1a7ad
Suresh Krishnaa R <progrsk24@gmail.com>
2021-04-13 11:43:29 +0530

    add crop recommendation flow component

--- src/Pages/CropRecommendationHome/Components/CropRecommendation/cropRecommendation.container.jsx
+++ src/Pages/CropRecommendationHome/Components/CropRecommendation/cropRecommendation.container.jsx
@@ -3,6 +3,43 @@ import CropRecommendationView from "./cropRecommendation.view";
 
 const CropRecommendation = () => {
   const [page, setPage] = useState(1);
+  const [envfactors, setEnvFactors] = useState({
+    rainfall: null,
+    temperature: null,
+    humidity: null,
+  });
+
+  const [npkValues, setNpkValues] = useState({
+    nitrogen: 92.2,
+    phosporous: 393.2,
+    potassium: 48.3,
+  });
+
+  const [location, setLocation] = useState(null);
+
+  const [crop, setCrop] = useState(null);
+
+  const handleEnvFactorsChange = (newenvfactors) => {
+    setEnvFactors(newenvfactors);
+  };
+
+  const handleCropChange = (predictedCrop) => {
+    setCrop(predictedCrop);
+  };
+
+  const handleLocationChange = () => {
+    let newenvfactors = {
+      rainfall: 103.38,
+      temperature: 200.94,
+      humidity: 67.15,
+    };
+    handleEnvFactorsChange(newenvfactors);
+    setLocation("CHENNAI");
+  };
+
+  const handleNpkChange = (newNpkvalues) => {
+    setNpkValues(newNpkvalues);
+  };
 
   const handlePageChange = (page) => {
     setPage(page);
@@ -10,7 +47,18 @@ const CropRecommendation = () => {
 
   return (
     <>
-      <CropRecommendationView page={page} handlePageChange={handlePageChange} />
+      <CropRecommendationView
+        page={page}
+        handlePageChange={handlePageChange}
+        envfactors={envfactors}
+        location={location}
+        crop={crop}
+        handleCropChange={handleCropChange}
+        handleEnvFactorsChange={handleEnvFactorsChange}
+        handleLocationChange={handleLocationChange}
+        npkValues={npkValues}
+        handleNpkChange={handleNpkChange}
+      />
     </>
   );
 };
