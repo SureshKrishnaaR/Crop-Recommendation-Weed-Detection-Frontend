dbf56e9c6bdd21b0b188bf34afd43a8b36fdd4c5
vishwa-jith <vishwajith567@gmail.com>
2021-04-14 11:43:43 +0530

    validate login and signup form

--- src/Pages/LandingPage/components/Login/login.container.jsx
+++ src/Pages/LandingPage/components/Login/login.container.jsx
@@ -13,11 +13,41 @@ const Login = () => {
     username: "",
     password: "",
   });
+  const [loginDetailsErrors, setLoginDetailsErrors] = useState({
+    username: "",
+    password: "",
+  });
 
   //handlers
   const handleLoginDetails = (event) => {
     const { name, value } = event.target;
+    validate(event);
     setLoginDetails({ ...loginDetails, [name]: value });
+  };
+
+  const validate = (event) => {
+    const { name, value } = event.target;
+    let error = "";
+    if (name === "username") {
+      const userNameRegex = /^[a-zA-Z0-9]+$/;
+      if (!value.length) {
+        error = "Username is required";
+      } else if (!value.match(userNameRegex)) {
+        error = "Alphanumeric characters only allowed";
+      }
+    } else if (name === "password") {
+      if (
+        !value.match(/[a-z]/g) ||
+        !value.match(/[A-Z]/g) ||
+        !value.match(/[0-9]/g) ||
+        !value.match(/[^a-zA-Z\d]/g)
+      ) {
+        error = "Invalid Password.";
+      } else if (!value.length >= 8) {
+        error = "Minimum 8 characters.";
+      }
+    }
+    setLoginDetailsErrors({ ...loginDetailsErrors, [name]: error });
   };
 
   const handleGoogleSignin = (username, password, full_name) => {
@@ -26,7 +56,6 @@ const Login = () => {
       password: password,
     };
     handleSubmit(loginobj);
-    setLoginDetails(loginobj);
   };
 
   const handleSubmit = (logindetails) => {
@@ -44,6 +73,7 @@ const Login = () => {
     <>
       <LoginPageView
         loginDetails={loginDetails}
+        loginDetailsErrors={loginDetailsErrors}
         setLoginDetails={setLoginDetails}
         handleLoginDetails={handleLoginDetails}
         handleSubmit={handleSubmit}
--- src/Pages/LandingPage/components/Register/register.container.jsx
+++ src/Pages/LandingPage/components/Register/register.container.jsx
@@ -12,10 +12,60 @@ const Register = () => {
     full_name: "",
     confirmPassword: "",
   });
+  const [registerdetailsErrors, setRegisterDetailsErrors] = useState({
+    username: "",
+    password: "",
+    full_name: "",
+    confirmPassword: "",
+  });
 
   const handleRegistrationDetails = (event) => {
     const { name, value } = event.target;
+    validate(event);
     setRegisterDetails({ ...registerdetails, [name]: value });
+  };
+
+  const validate = (event) => {
+    const { name, value } = event.target;
+    let error = "";
+    if (name === "username") {
+      const userNameRegex = /^[a-zA-Z0-9]+$/;
+      if (!value.length) {
+        error = "Username is required";
+      } else if (!value.match(userNameRegex)) {
+        error = "Alphanumeric characters only allowed";
+      }
+    } else if (name === "full_name") {
+      const nameRegex = /^[a-zA-Z\-]+$/;
+      if (!value.length) {
+        error = "Name is required";
+      } else if (!value.match(nameRegex)) {
+        error = "Alphabets only allowed";
+      }
+    } else if (name === "password") {
+      if (!value.length) {
+        error = "Name is required";
+      } else if (!value.match(/[a-z]/g)) {
+        error = "At least 1 lowercase character.";
+      } else if (!value.match(/[A-Z]/g)) {
+        error = "At least 1 uppercase character.";
+      } else if (!value.match(/[0-9]/g)) {
+        error = "At least 1 digit.";
+      } else if (!value.match(/[^a-zA-Z\d]/g)) {
+        error = "At least 1 special character.";
+      } else if (!value.length >= 8) {
+        error = "Minimum 8 characters.";
+      }
+    } else if (name === "confirmPassword") {
+      if (!value.length) {
+        error = "Confirm Password is required.";
+      } else if (!registerdetails.password.length) {
+        error = "Password is empty.";
+      } else if (value !== registerdetails.password) {
+        error = "Password doesn't match.";
+      }
+    }
+    setRegisterDetailsErrors({ ...registerdetailsErrors, [name]: error });
   };
 
   const handleGoogleSignin = (username, password, full_name) => {
@@ -25,25 +75,27 @@ const Register = () => {
       confirmPassword: password,
       full_name: full_name,
     };
-    setRegisterDetails(registerobj);
     handleSubmit(registerobj);
   };
 
   const handleSubmit = (registerobj) => {
-    signUp(registerobj)
-      .then((res) => {
-        console.log(res);
-        history.push("/");
-      })
-      .catch((err) => {
-        console.log(err);
-      });
+    if (registerobj.confirmPassword === registerobj.password) {
+      signUp(registerobj)
+        .then((res) => {
+          console.log(res);
+          history.push("/");
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
   };
 
   return (
     <>
       <RegisterPageView
         registerdetails={registerdetails}
+        registerdetailsErrors={registerdetailsErrors}
         setRegisterDetails={setRegisterDetails}
         handleRegistrationDetails={handleRegistrationDetails}
         handleSubmit={handleSubmit}
