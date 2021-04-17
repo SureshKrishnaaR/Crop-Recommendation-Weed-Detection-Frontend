2deb772f75b9e1e5b317435d58fab46883d963b9
Suresh Krishnaa R <progrsk24@gmail.com>
2021-04-17 23:57:33 +0530

    add choose location component to the app

---
+++ src/Pages/CropRecommendationHome/Components/CropRecommendation/Components/ChooseLocation/chooseLocation.container.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+
+import ChooseLocationView from "./chooseLocation.view";
+import { getWeatherDetails } from "../../../../../../utils/requests";
+
+const ChooseLocation = ({
+  page,
+  location,
+  locationval,
+  handlePageChange,
+  envfactors,
+  handleCropChange,
+  crop,
+  handleLocationChange,
+  handleNpkClick,
+  handleEnvFactorsChange,
+  handleLocationvalChange,
+  handleStatesAPI,
+  handleStateChange,
+  handleDistrictsAPI,
+  allstates,
+  alldistricts,
+  chosenstate,
+  setAllStates,
+  setChosenState,
+
+  approach,
+}) => {
+  const handleWeatherUpdate = () => {
+    getWeatherDetails(locationval)
+      .then((resp) => {
+        handleEnvFactorsChange(resp);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  return (
+    <>
+      <ChooseLocationView
+        page={page}
+        handlePageChange={handlePageChange}
+        envfactors={envfactors}
+        location={location}
+        locationval={locationval}
+        crop={crop}
+        handleCropChange={handleCropChange}
+        handleEnvFactorsChange={handleEnvFactorsChange}
+        handleLocationChange={handleLocationChange}
+        handleLocationvalChange={handleLocationvalChange}
+        allstates={allstates}
+        alldistricts={alldistricts}
+        handleStatesAPI={handleStatesAPI}
+        handleDistrictsAPI={handleDistrictsAPI}
+        chosenstate={chosenstate}
+        handleStateChange={handleStateChange}
+        setChosenState={setChosenState}
+        approach={approach}
+        handleWeatherUpdate={handleWeatherUpdate}
+      />
+    </>
+  );
+};
+
+export default ChooseLocation;
