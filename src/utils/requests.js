8bc242d5e9e54f1e6738fb5a7ddfe8cdaf262284
Suresh Krishnaa R <progrsk24@gmail.com>
2021-04-18 00:14:18 +0530

    add api requests for all the components in the application

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -63,3 +63,55 @@ export const updateProfile = async (profileDetails) => {
   });
   return response.data;
 };
+
+export const getWeatherDetails = async (district) => {
+  let response = await axios.get(
+    baseUrl + `/weather?district_name=${district}`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+  return response.data;
+};
+
+export const predictCrop = async (district) => {
+  let response = await axios.get(
+    baseUrl + `/A1/recommendCrop?district=${district}`,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+  console.log(response);
+  return response.data;
+};
+
+export const predictNpk = async (crop, district) => {
+  let response = await axios.post(
+    baseUrl + `/A1/recommendNPK?district=${district}`,
+    { crop: crop.toLowerCase() },
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+  return response.data;
+};
+
+export const predictFertilizer = async (reqdetails, district) => {
+  console.log(reqdetails);
+  let response = await axios.post(
+    baseUrl + `/A1/recommendFertilizer?district=${district}`,
+    reqdetails,
+    {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    }
+  );
+  return response.data;
+};
