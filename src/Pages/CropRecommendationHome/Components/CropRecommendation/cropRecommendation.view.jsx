b0f5d19d68a9b92bd3215d1928dbf7e96010f695
Suresh Krishnaa R <progrsk24@gmail.com>
2021-04-19 19:58:14 +0530

    add back button to the recommendation components

--- src/Pages/CropRecommendationHome/Components/CropRecommendation/cropRecommendation.view.jsx
+++ src/Pages/CropRecommendationHome/Components/CropRecommendation/cropRecommendation.view.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { Button, IconButton, useMediaQuery } from "@material-ui/core";
+import { motion } from "framer-motion";
+import { useTheme } from "@material-ui/core/styles";
+import KeyboardBackspaceIcon from "@material-ui/icons/KeyboardBackspace";
 
 import FertilizerQuestion from "./components/FertilizerQuestion";
 import ChooseLocation from "./components/ChooseLocation";
@@ -41,6 +45,8 @@ const CropRecommendationView = ({
   approach,
   handleApproachChange,
 }) => {
+  const mediatheme2 = useTheme();
+  const matches = useMediaQuery(mediatheme2.breakpoints.up("sm"));
   return (
     <>
       <div
@@ -48,8 +54,50 @@ const CropRecommendationView = ({
           background: "#ffffff",
           backgroundImage: "linear-gradient(315deg, #ffffff 0%, #d7e1ec 74%)",
           height: "calc(100vh - 65px)",
+          alignItems: "center",
+          justifyContent: "center",
+          overflow: "hidden",
+          position: "relative",
         }}
       >
+        {page !== 1 && (
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            style={{
+              marginTop: "20px",
+              position: "absolute",
+              top: "20px",
+              left: matches ? "50px" : "40vw",
+            }}
+          >
+            {matches ? (
+              <IconButton
+                color="primary"
+                style={{
+                  boxShadow:
+                    "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset",
+                }}
+              >
+                <KeyboardBackspaceIcon
+                  onClick={() => {
+                    handlePageChange(page - 1);
+                  }}
+                />
+              </IconButton>
+            ) : (
+              <Button
+                color="primary"
+                variant="outlined"
+                onClick={() => {
+                  handlePageChange(page - 1);
+                }}
+              >
+                BACK
+              </Button>
+            )}
+          </motion.div>
+        )}
         {page === 1 ? (
           <FertilizerQuestion
             page={page}
